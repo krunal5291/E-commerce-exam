3b3c0ce1f467a2d619f19f63ff08d222809ee295
krunal5291 <krunalkalathiya11@gmail.com>
2024-08-20 11:20:39 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const { mongoose } = require("mongoose");
+require("dotenv").config()
+const connection = ()=>{
+    mongoose.connect("mongodb://localhost:27017")
+    console.log("db connected");
+}
+
+module.exports = {connection}
+
